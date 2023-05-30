8216e6bea3611287bde56308f9205b8f3c1399f2
LauritsSchou <laurits.schou@gmail.com>
2023-05-30 12:34:53 +0200

    exercise-00

--- environments/environment-00/script.js
+++ environments/environment-00/script.js
@@ -1,1 +1,11 @@
 "use strict";
+window.addEventListener("load", initApp);
+function initApp() {
+  document.querySelector("#result_failure").setAttribute("hidden", true);
+  document.querySelector("#result_success").setAttribute("hidden", true);
+  document.querySelector("#btn-knap").addEventListener("click", clicked);
+}
+function clicked() {
+  document.querySelector("#result_success").removeAttribute("hidden");
+  console.log("det virker");
+}
