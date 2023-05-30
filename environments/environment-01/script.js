40976b258b76ff108e4a24dd98c3e3de989b6c00
LauritsSchou <laurits.schou@gmail.com>
2023-05-30 13:32:01 +0200

    solved first part

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -1,2 +1,21 @@
 "use strict";
-
+window.addEventListener("load", initApp);
+let users = [];
+async function initApp() {
+  await getUsers();
+  showUsers(users);
+}
+async function getUsers() {
+  const response = await fetch("users.json");
+  const json = await response.json();
+  users.push(json);
+}
+function showUsers(users) {
+  const userlist = document.querySelector("#userlist");
+  userlist.innerHTML = "";
+  for (const user of users) {
+    let userHTML = /*html*/ `
+    <li> User name: ${user.name} User status: ${user.active}</li>`;
+    userlist.insertAdjacentHTML("beforeend", userHTML);
+  }
+}
