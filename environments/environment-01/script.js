9afcdd2d4cc4ea02649291d1d6272916dc59d190
LauritsSchou <laurits.schou@gmail.com>
2023-05-31 13:35:07 +0200

    solved

--- environments/environment-01/script.js
+++ environments/environment-01/script.js
@@ -8,14 +8,24 @@ async function initApp() {
 async function getUsers() {
   const response = await fetch("users.json");
   const json = await response.json();
-  users.push(json);
+  users = json;
 }
-function showUsers(users) {
+function showUsers(listOfUsers) {
+  const adminUsers = listOfUsers.filter(sortUsersByAdmin);
   const userlist = document.querySelector("#userlist");
   userlist.innerHTML = "";
-  for (const user of users) {
-    let userHTML = /*html*/ `
-    <li> User name: ${user.name} User status: ${user.active}</li>`;
-    userlist.insertAdjacentHTML("beforeend", userHTML);
+  for (const user of adminUsers) {
+    if (user.active === true) {
+      let userHTML = /*html*/ `
+    <li> User name: ${user.name} User status: Active</li>`;
+      userlist.insertAdjacentHTML("beforeend", userHTML);
+    } else {
+      let userHTML = /*html*/ `
+    <li> User name: ${user.name} User status: Passive</li>`;
+      userlist.insertAdjacentHTML("beforeend", userHTML);
+    }
   }
+}
+function sortUsersByAdmin(user) {
+  return user.role === "admin";
 }
