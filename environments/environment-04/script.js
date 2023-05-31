3b9853c676b844123ce10edc925a950fce770455
LauritsSchou <laurits.schou@gmail.com>
2023-05-31 18:17:23 +0200

    exercise 09 solved

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,22 @@
 "use strict";
+import { teachers } from "./teachers.js";
+window.addEventListener("load", initApp);
+function initApp() {
+  showTeachers();
+}
+function showTeachers() {
+  const teachersList = document.querySelector("#teachers-list");
+  teachersList.innerHTML = "";
+  for (const teacher of teachers) {
+    const teacherListHTML = /*html*/ `
+    <li>${teacher.name} ${teacher.email}</li>
+    `;
+    teachersList.insertAdjacentHTML("beforeend", teacherListHTML);
+  }
+}
+function sortByName(teachers) {
+  return teachers.sort((a, b) => a.name.localeCompare(b.name));
+}
+function sortByEmail(teachers) {
+  return teachers.sort((a, b) => a.email.localeCompare(b.email));
+}
