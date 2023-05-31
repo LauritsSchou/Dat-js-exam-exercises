20d52faf6615d8961f656d08cb654f971dfcc77c
LauritsSchou <laurits.schou@gmail.com>
2023-05-31 20:12:12 +0200

    exercise 10 solved

--- environments/environment-04/script.js
+++ environments/environment-04/script.js
@@ -1,1 +1,28 @@
 "use strict";
+import { teachers } from "./teachers.js";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  showTeachers();
+}
+
+function showTeachers() {
+  const teachersList = document.querySelector("#teachers-list");
+  teachersList.innerHTML = "";
+
+  for (const teacher of teachers) {
+    const teachersListHTML = /*html*/ `
+    <li>Name: ${teacher.name}. <br>Email: ${teacher.email}</li>
+    `;
+    teachersList.insertAdjacentHTML("beforeend", teachersListHTML);
+  }
+}
+function addTeacher(name, email) {
+  const newTeacher = {
+    name: name,
+    email: email,
+  };
+  teachers.push(newTeacher);
+  showTeachers();
+}
