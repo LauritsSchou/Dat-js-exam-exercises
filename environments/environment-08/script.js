4a2d37f549923461d80ef96e6f34e229833f55d8
LauritsSchou <laurits.schou@gmail.com>
2023-05-31 11:45:37 +0200

    environment 08 exam example solved

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,27 @@
 "use strict";
+let songs = [];
+window.addEventListener("load", initApp);
+async function initApp() {
+  await getPlaylist();
+  showSongs(songs);
+}
+async function getPlaylist() {
+  const response = await fetch("playlist.json");
+  const json = await response.json();
+  console.log(json);
+  songs = json;
+}
+function showSongs(songs) {
+  document.querySelector("#songlist").innerHTML = "";
+  for (const song of songs) {
+    let songlistHTML = /*html*/ `
+        <li>${song.artist} ${song.title} ${song.duration} <button>Remove</button></li>
+        `;
+    document.querySelector("#songlist").insertAdjacentHTML("beforeend", songlistHTML);
+    document.querySelector("#songlist li:last-child button").addEventListener("click", () => removeClicked(song));
+  }
+}
+function removeClicked(songToRemove) {
+  songs = songs.filter((song) => song !== songToRemove);
+  showSongs(songs);
+}
