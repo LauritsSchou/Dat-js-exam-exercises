ab8ca138f6187bebecbfcc3ccba706d95e1256bd
LauritsSchou <laurits.schou@gmail.com>
2023-06-01 11:51:18 +0200

    exercise 19 solved

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,35 @@
 "use strict";
+const students = [];
+window.addEventListener("load", initApp);
+
+function initApp() {
+  document
+    .querySelector("#create-student-form")
+    .addEventListener("submit", addStudent);
+}
+function addStudent(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = form.age.value;
+  addStudentClicked(name, email, age);
+}
+function addStudentClicked(name, email, age) {
+  const newStudent = {
+    name: name,
+    email: email,
+    age: age,
+  };
+  console.log(checkStudentMail(newStudent));
+  if (
+    checkStudentMail(newStudent)[0].length >= 4 &&
+    checkStudentMail(newStudent)[1] === "stud.kea.dk"
+  ) {
+    students.push(newStudent);
+  }
+  console.log(students);
+}
+function checkStudentMail(student) {
+  return student.email.split("@");
+}
