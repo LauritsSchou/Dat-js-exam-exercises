7acb22a25b54ef93c716c52c6817ab912b17b28b
LauritsSchou <laurits.schou@gmail.com>
2023-06-01 11:12:31 +0200

    exercise 14 solved

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,18 @@
 "use strict";
+import { courses } from "./courses.js";
+window.addEventListener("load", initApp);
+
+function initApp() {
+  sortCoursesByDate(courses);
+  const coursesList = document.querySelector("#courses-list");
+  coursesList.innerHTML = "";
+  for (const course of courses) {
+    const coursesListHTML = /*html*/ `
+    <li>${course.name} ${course.startDate} ${course.ectsPoints}</li>
+    `;
+    coursesList.insertAdjacentHTML("beforeend", coursesListHTML);
+  }
+}
+function sortCoursesByDate(courses) {
+  courses.sort((a, b) => new Date(a.startDate) - new Date(b.startDate));
+}
