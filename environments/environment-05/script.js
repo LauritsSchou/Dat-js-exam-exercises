050062ee2b243a3826a892d3406f79cbc03896fb
LauritsSchou <laurits.schou@gmail.com>
2023-06-01 10:34:45 +0200

    exercise 12 solved

--- environments/environment-05/script.js
+++ environments/environment-05/script.js
@@ -1,1 +1,24 @@
 "use strict";
+import { courses } from "./courses.js";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  showCourses();
+}
+
+function showCourses() {
+  const coursesList = document.querySelector("#courses-list");
+  coursesList.innerHTML = "";
+  compareCourses(courses);
+  for (const course of courses) {
+    const coursesListHTML = /*html*/ `
+    <li>${course.name}${course.ectsPoints}</li>
+    
+    `;
+    coursesList.insertAdjacentHTML("beforeend", coursesListHTML);
+  }
+}
+function compareCourses() {
+  courses.sort((a, b) => a.ectsPoints - b.ectsPoints);
+}
