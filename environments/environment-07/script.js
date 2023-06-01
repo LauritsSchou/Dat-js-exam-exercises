1d1a21f8ed0fa0abbd70948c639c86016f287f63
LauritsSchou <laurits.schou@gmail.com>
2023-06-01 12:05:20 +0200

    exercise 20 solved

--- environments/environment-07/script.js
+++ environments/environment-07/script.js
@@ -1,1 +1,44 @@
 "use strict";
+window.addEventListener("load", initApp);
+const students = [];
+function initApp() {
+  showStudents();
+  document
+    .querySelector("#create-student-form")
+    .addEventListener("submit", addStudent);
+}
+
+function addStudent(event) {
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const age = form.age.value;
+  addStudentClicked(name, email, age);
+}
+
+function addStudentClicked(name, email, age) {
+  const newStudent = {
+    name: name,
+    email: email,
+    age: age,
+  };
+  students.push(newStudent);
+  showStudents(students);
+  console.log(students);
+}
+function showStudents() {
+  students.sort((a, b) => a.age - b.age);
+  const studentsTable = document.querySelector("#students-table-body");
+  studentsTable.innerHTML = "";
+  for (const student of students) {
+    const studentsTableHTML = /*html*/ `
+     <tr>
+            <td>${student.name}</td>
+            <td>${student.email}</td>
+            <td>${student.age}</td>
+          </tr>
+    `;
+    studentsTable.insertAdjacentHTML("beforeend", studentsTableHTML);
+  }
+}
