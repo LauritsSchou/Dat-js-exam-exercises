d146ea6bd1fe4d73bcafa15d9cd100d3215010c1
LauritsSchou <laurits.schou@gmail.com>
2023-06-01 12:36:38 +0200

    exercise 22

--- environments/environment-08/script.js
+++ environments/environment-08/script.js
@@ -1,1 +1,34 @@
 "use strict";
+let playlist = [];
+window.addEventListener("load", initApp);
+async function initApp() {
+  await getPlaylist();
+  showSongs(playlist);
+}
+
+async function getPlaylist() {
+  const response = await fetch("playlist.json");
+  const data = await response.json();
+  playlist = data;
+}
+function showSongs(playlist) {
+  const songList = document.querySelector("#songlist");
+  songList.innerHTML = "";
+  for (const song of playlist) {
+    const songListHTML =
+      /*html*/
+      `
+    <li>${song.artist} ${song.title} ${song.duration}<button>Remove</button></li>
+    `;
+    songList.insertAdjacentHTML("beforeend", songListHTML);
+    document
+      .querySelector("#songlist li:last-child button")
+      .addEventListener("click", () => removeClicked(song.title));
+  }
+}
+
+function removeClicked(songName) {
+  console.log(songName);
+  playlist = playlist.filter((song) => song.title !== songName);
+  showSongs(playlist);
+}
